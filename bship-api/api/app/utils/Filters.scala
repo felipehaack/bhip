95d62e472fa37122e2e7bc8b66146227781224eb
Felipe Haack Schmitz <felipe.haackschmitz@gmail.com>
2017-01-26 14:14:15 -0200

    Added to library the filters dependency Added to application conf settings to CORS works only for 9010 Added Filters class (play specification) Removed unnecessary changes

--- bship-api/api/app/utils/Filters.scala
+++ bship-api/api/app/utils/Filters.scala
@@ -1,41 +1,7 @@
-/*
 package utils
 
-import javax.inject.{Inject, Singleton}
+import javax.inject.Inject
+import play.filters.cors.CORSFilter
+import play.api.http.DefaultHttpFilters
 
-import play.api.http.HttpFilters
-
-@Singleton
-class Filters @Inject()(
-                         loggingFilter: LoggingFilter
-                       ) extends HttpFilters {
-  val filters = Seq(loggingFilter)
-}
-
-
-import javax.inject.{Inject, Singleton}
-
-import akka.stream.Materializer
-import play.api.mvc.{Filter, RequestHeader, Result}
-
-import scala.concurrent.{ExecutionContext, Future}
-
-@Singleton
-class LoggingFilter @Inject()(
-                               implicit val mat: Materializer,
-                               implicit val ec: ExecutionContext
-                             ) extends Filter with Logging {
-
-  def apply(nextFilter: RequestHeader => Future[Result])
-           (requestHeader: RequestHeader): Future[Result] = {
-
-    val startTime = System.currentTimeMillis
-    nextFilter(requestHeader).map { result =>
-      val endTime = System.currentTimeMillis
-      val requestTime = endTime - startTime
-      log.info(s"${requestHeader.method} ${requestHeader.uri} took ${requestTime}ms and returned ${result.header.status}")
-      result.withHeaders("Request-Time" -> requestTime.toString)
-    }
-  }
-}
-*/
+class Filters @Inject()(corsFilter: CORSFilter) extends DefaultHttpFilters(corsFilter)
--- bship-api/api/build.sbt
+++ bship-api/api/build.sbt
@@ -4,3 +4,5 @@ libraryDependencies ++= Seq(
   "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0-M1",
   "org.mockito" % "mockito-all" % "1.10.19"
 )
+
+libraryDependencies += filters
