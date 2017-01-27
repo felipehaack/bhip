f12ce118817b819702f366acc2a549e5ab7667ad
Felipe Haack Schmitz <felipe.haackschmitz@gmail.com>
2017-01-27 19:15:51 -0200

    Cors and Frontend:
    Removed allowed origins from localhost, because, each player will has the backend on pc and it will be a different ip address
    Formatted all front end code with intellij specification
    Added svg file export to build folder
    Removed unnecessary comments
    Added more custom css for angular material and default color to be used on all the project
    Removed unnecessary dependencies on app module and add material and message
    Added challenge template for modal to request a challenge to another player
    Improvements on home controller

--- bship-site/gulpfile.js
+++ bship-site/gulpfile.js
@@ -13,138 +13,123 @@ var minifyCss = require("gulp-minify-css");
 var del = require("del");
 
 var paths = {
-	tscripts: {
-		src: ["typings/tsd.d.ts", "app/app.consts.ts", "app/app.ts", "app/**/*.ts"],
-		dest: "app/build"
-	},
-	html: ["app/**/*.html", "index.html"],
-	mainHtml: "index.html",
-	sass: {
-		src: ["assets/styles/**/*.scss"],
-		dest: "./build/styles"
-	},
-	fonts: {
-		src: ["bower_components/bootstrap/fonts/*.{eot,svg,ttf,woff,woff2}"],
-		dest: "./build/fonts"
-	},
-	imgs: {
-		src: ["assets/imgs/*.{png,jpeg,jpg}"],
-		dest: "./build/imgs"
-	},
-	dist: "./build"
+    tscripts: {
+        src: ["typings/tsd.d.ts", "app/app.consts.ts", "app/app.ts", "app/**/*.ts"],
+        dest: "app/build"
+    },
+    html: ["app/**/*.html", "index.html"],
+    mainHtml: "index.html",
+    sass: {
+        src: ["assets/styles/**/*.scss"],
+        dest: "./build/styles"
+    },
+    fonts: {
+        src: ["bower_components/bootstrap/fonts/*.{eot,svg,ttf,woff,woff2}"],
+        dest: "./build/fonts"
+    },
+    imgs: {
+        src: ["assets/imgs/*.{png,jpeg,jpg,svg}", "assets/imgs/icons/*.svg"],
+        dest: "./build/imgs"
+    },
+    dist: "./build"
 };
 
 gulp.task("default", function () {
 
 });
 
-
-// ** Running ** //
-
-
-//gulp.task("buildrun", function (cb) {
-//	runseq("build", "run", cb);
-//});
-
-// ** Watching ** //
-
-
 gulp.task("watch", ["serve"], function () {
-	gulp.watch(paths.tscripts.src, ["compile:typescript", browserSync.reload]).on("change", reportChange);
-	gulp.watch(paths.html, ["html", browserSync.reload]).on("change", reportChange);
-	gulp.watch(paths.mainHtml, ["bower", browserSync.reload]).on("change", reportChange);
-	gulp.watch(paths.sass.src, ["compile:sass", browserSync.reload]).on("change", reportChange);
+    gulp.watch(paths.tscripts.src, ["compile:typescript", browserSync.reload]).on("change", reportChange);
+    gulp.watch(paths.html, ["html", browserSync.reload]).on("change", reportChange);
+    gulp.watch(paths.mainHtml, ["bower", browserSync.reload]).on("change", reportChange);
+    gulp.watch(paths.sass.src, ["compile:sass", browserSync.reload]).on("change", reportChange);
 });
 
 // ** Compilation ** //
 gulp.task("build:prod", ["build"], function (cb) {
-	runseq("minify", cb);
+    runseq("minify", cb);
 });
-gulp.task("build", ["compile:typescript", "compile:sass", "bower", "html", "copy:fonts", "copy:imgs"]);
-gulp.task("compile:typescript", function () {
-	var tsResult = gulp
-		.src(paths.tscripts.src)
-		.pipe(sourcemaps.init())
-		.pipe(tsc({
-		module: "amd",
-		target: "ES5",
-		declarationFiles: false,
-		/*emitError: false,*/
-		emitDecoratorMetadata: true
-	}));
-		
-	//tsResult.dts.pipe(gulp.dest("./def"))
-	return tsResult.js
-		.pipe(concat("app.js"))
-		.pipe(ngAnnotate())
-		.pipe(sourcemaps.write("."))
-		.pipe(gulp.dest("./build/scripts"));
 
+gulp.task("build", ["compile:typescript", "compile:sass", "bower", "html", "copy:fonts", "copy:imgs"]);
+
+gulp.task("compile:typescript", function () {
+    var tsResult = gulp
+        .src(paths.tscripts.src)
+        .pipe(sourcemaps.init())
+        .pipe(tsc({
+            module: "amd",
+            target: "ES5",
+            declarationFiles: false,
+            /*emitError: false,*/
+            emitDecoratorMetadata: true
+        }));
+
+    return tsResult.js
+        .pipe(concat("app.js"))
+        .pipe(ngAnnotate())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("./build/scripts"));
 });
 
 gulp.task("compile:sass", function () {
-	gulp.src(paths.sass.src)
-		.pipe(sourcemaps.init())
-		.pipe(sass())
-		.pipe(sourcemaps.write("."))
-		.pipe(gulp.dest(paths.sass.dest));
+    gulp.src(paths.sass.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest(paths.sass.dest));
 });
 
 gulp.task("html", function () {
-	gulp.src(["app/**/*.html"])
-		.pipe(gulp.dest(paths.dist));
+    gulp.src(["app/**/*.html"])
+        .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task("minify", function () {
-	return gulp.src("build/index.html")
-		.pipe(usemin({
-		assetsDir: "build",
-		css: [minifyCss(), "concat"],
-		js: [uglify(), "concat"]
-	}))
-		.pipe(gulp.dest("build"));
+    return gulp.src("build/index.html")
+        .pipe(usemin({
+            assetsDir: "build",
+            css: [minifyCss(), "concat"],
+            js: [uglify(), "concat"]
+        }))
+        .pipe(gulp.dest("build"));
 });
 
 gulp.task("bower", function () {
-	gulp.src(paths.mainHtml)
-		.pipe(wiredep())
-		.pipe(gulp.dest(paths.dist));
+    gulp.src(paths.mainHtml)
+        .pipe(wiredep())
+        .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task("copy:fonts", function () {
-	gulp.src(paths.fonts.src)
-		.pipe(gulp.dest(paths.fonts.dest));
+    gulp.src(paths.fonts.src)
+        .pipe(gulp.dest(paths.fonts.dest));
 });
 
 gulp.task("copy:imgs", function () {
-	gulp.src(paths.imgs.src)
-		.pipe(gulp.dest(paths.imgs.dest));
+    gulp.src(paths.imgs.src)
+        .pipe(gulp.dest(paths.imgs.dest));
 });
 
 // ** Clean ** //
 gulp.task("clean", function (cb) {
-	del([paths.dist], cb);
+    del([paths.dist], cb);
 });
 
-
 // ** Utils ** //
-
 gulp.task("serve", ["build:prod"], function (done) {
-
-	browserSync({
-		open: false,
-		port: 8000,
-		server: {
-			baseDir: [paths.dist],
-			middleware: function (req, res, next) {
-				res.setHeader("Access-Control-Allow-Origin", "*");
-				next();
-			}
-		}
-	}, done);
-
+    browserSync({
+        open: false,
+        port: 9010,
+        server: {
+            baseDir: [paths.dist],
+            middleware: function (req, res, next) {
+                res.setHeader("Access-Control-Allow-Origin", "*");
+                next();
+            }
+        }
+    }, done);
 });
 
 function reportChange(event) {
-	console.log("File " + event.path + " was " + event.type + ", running tasks...");
+    console.log("File " + event.path + " was " + event.type + ", running tasks...");
 }
